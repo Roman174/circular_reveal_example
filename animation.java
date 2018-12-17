692f79ff7ebed68d53bd7c3553c6d32eda606a7f
Роман Прокопьев <174.pra@gmail.com>
2018-12-17 08:35:48 +0500

    add background aninmation

--- animation.java
+++ animation.java
@@ -7,7 +7,13 @@ private void show(View v) {
     int height = v.getHeight();
 
     float finalRadius = Math.max(width, height) / 2 + Math.max(width, height);
+
     Animator anim = ViewAnimationUtils.createCircularReveal(v, cx, cy, 0, finalRadius);
+    startBackgroundColorAnimation(v,
+            getResources().getColor(R.color.colorPrimary),
+            getResources().getColor(R.color.colorWhite),
+            500);
+
     anim.setDuration(500);
     anim.start();
 }
@@ -22,6 +28,16 @@ private void hide(View v) {
 
     float finalRadius = Math.max(width, height) / 2 + Math.max(width, height);
     Animator anim = ViewAnimationUtils.createCircularReveal(v, cx, cy, finalRadius, 0);
-    anim.setDuration(500);
+    anim.setDuration(250);
+    anim.start();
+}
+
+private void startBackgroundColorAnimation(final View view, int startColor, int endColor,
+                                            long duration) {
+    ValueAnimator anim = new ValueAnimator();
+    anim.setIntValues(startColor, endColor);
+    anim.setEvaluator(new ArgbEvaluator());
+    anim.setDuration(duration);
+    anim.addUpdateListener(valueAnimator -> view.setBackgroundColor((Integer) valueAnimator.getAnimatedValue()));
     anim.start();
 }
